c0e459f02ae64e9c1d52ae13e510fbd32b489629
Param Rajpura <param.rajpura92@gmail.com>
2023-12-10 15:17:12 +0000

    Updated readme and streamlit app description.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,17 @@ hv.output(size=300)
 
 This application helps visualise the variable categories for each axis of the XAI4BCI design space.
 
+A chord diagram is a graphical method of displaying the relationships between data in a matrix. 
+It is used to visualize the connections or relationships between a finite set of entities. 
+The entities are represented as circular segments, and the connections between them are illustrated using ribbons or chords. 
+The entities are represented as circles around the perimeter of the diagram, each circle corresponds to an individual entity. 
+The chords are the arcs connecting pairs of entities. The width of the chord may be used to represent the strength or frequency 
+of the relationship between the connected entities. The input is a matrix, where the rows and columns correspond to the entities, 
+and the matrix entries represent the strength or frequency of the relationships between them. The color represents the different 
+entities around the disk. The colors are chosen arbitrarily.
+
 Click on the circular symbol adjacent to each design space variable category to 
 find the strength of correlated works with other design space variables.
-
-The width of each connection in the chord diagram denotes the number of published studies at the intersection of those two variable categories.
 """
 
 df = pd.read_csv('chord.csv')
